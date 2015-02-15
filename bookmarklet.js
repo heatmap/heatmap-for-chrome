20e38a668ba7b7e8908a119cb6cdc433a1f28106
Max Mathieu <max@heatmap.me>
2015-02-14 23:55:01 -0500

    Fixed #1

---
+++ bookmarklet.js
@@ -0,0 +1,1 @@
+(function(){var s=document.createElement('script');s.type='text/javascript';s.src='//u.heatmap.it/bookmark.js';(top.document.body || top.document.getElementsByTagName('head')[0]).appendChild(s);})();
